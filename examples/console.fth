690148acd1badafc819a5a6d740afd77efe77072
Xie Yuheng <xyheme@gmail.com>
2024-12-28 21:01:19 +0800

    [value] value_print -- fix for float

---
+++ examples/console.fth
@@ -0,0 +1,14 @@
+null print newline
+true print newline
+false print newline
+
+1 2 add print newline
+-1 -2 add print newline
+
+0.0 0.0 fadd print newline
+0.1 0.2 fadd print newline
+1.0 2.0 fadd print newline
+1.1 2.2 fadd print newline
+
+1. print newline
+2. print newline
--- src/value/value.c
+++ src/value/value.c
@@ -28,7 +28,16 @@ value_print(value_t value, file_t *file) {
     }
 
     if (is_xfloat(value)) {
-        fprintf(file, "%f", to_double(value));
+        char buffer[64];
+        sprintf(buffer, "%.17g", to_double(value));
+        if (!string_has_char(buffer, '.')) {
+            size_t end = string_length(buffer);
+            buffer[end] = '.';
+            buffer[end + 1] = '0';
+            buffer[end + 2] = '\0';
+        }
+
+        fprintf(stdout, "%s", buffer);
         return;
     }
 
