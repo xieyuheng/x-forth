22544271c1bc4e39aca213d0741893e6e185269f
Xie Yuheng <xyheme@gmail.com>
2025-01-05 10:30:34 +0800

    step_vm -- fix memory leak

--- src/lang/run.c
+++ src/lang/run.c
@@ -5,14 +5,24 @@ step_vm(vm_t *self) {
     if (stack_is_empty(self->return_stack)) return;
 
     frame_t *frame = stack_pop(self->return_stack);
-    if (frame_is_finished(frame)) return;
+    if (frame_is_finished(frame)) {
+        frame_destroy(&frame);
+        return;
+    }
 
     op_t *op = frame_fetch_op(frame);
+
     // proper tail-call = do not push finished frame.
     bool finished = frame_is_finished(frame);
-    if (!finished) stack_push(self->return_stack, frame);
+    if (!finished) {
+        stack_push(self->return_stack, frame);
+    }
+
     execute_operation(self, frame, op);
-    if (finished) frame_destroy(&frame);
+
+    if (finished) {
+        frame_destroy(&frame);
+    }
 }
 
 void
