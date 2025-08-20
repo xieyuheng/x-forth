b26293aac3d6e4e3c2512bce034570d8fdbf4f43
Xie Yuheng <xyheme@gmail.com>
2025-08-21 03:22:02 +0800

    up

---
+++ scripts/run-examples.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+bin=./bin/x-forth
+ext=fth
+dir=examples
+
+for file in $(find $dir -name "*.${ext}" -not -name "*.test.${ext}" -not -name "*.error.${ext}" -not -name "*.play.${ext}"); do
+    echo "[run] $file"
+    ${bin} run $file
+done
+
+for file in $(find $dir -name "*.test.${ext}"); do
+    echo "[test] $file"
+    ${bin} run $file > $file.out
+done
+
+for file in $(find $dir -name "*.error.${ext}"); do
+    echo "[error] $file"
+    ${bin} run $file 2> $file.err || true
+done
